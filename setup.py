ad4e5d009055b604412d518a220a1c003e49dd79
donglaiw <weiddoonngglai@gmail.com>
2024-02-17 14:05:35 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,40 @@
-"""Package installation"""
+import os, sys
+from distutils.sysconfig import get_python_inc
+from setuptools import setup, find_packages, Extension
+import numpy as np
 
-from setuptools import setup, find_packages
+def getInclude():
+    dirName = get_python_inc()
+    return [dirName, os.path.dirname(dirName), np.get_include()]
 
+def getExtension():
+    extensions = []
+    extensions += [Extension(
+            'seglib.region_graph',
+            sources=['seglib/region_graph.pyx', 'seglib/cpp/region_graph.cpp'],
+            language='c++',
+            extra_link_args=['-std=c++11'],
+            extra_compile_args=['-std=c++11'])]
+    return extensions
 
-def setup_package():
+def setup_package(no_cython=True):
+    __version__ = '0.1'
+    url = 'https://github.com/donglaiw/emutil'
+
+def setup_package(no_cython=True):
     """Required packages"""
     __version__ = "0.1"
     url = "https://github.com/PytorchConnectomics/seglib"
+    exts = [] 
+    package_data = {}
+    if not no_cython:
+        from Cython.Build import cythonize
+        exts = cythonize(getExtension())
+        package_data = {'': [
+                'emu/seg/cpp/*.h',
+                'emu/seg/cpp/*.cpp',
+                'emu/seg/*.pyx',
+            ]}
     setup(
         name="erl",
         description="Useful functions for segmentation",
@@ -14,11 +42,18 @@ def setup_package():
         url=url,
         license="MIT",
         author="Donglai Wei",
-        install_requires=["scipy", "numpy", "h5py", "imageio", "argparse"],
-        packages=find_packages(),
+        install_requires=["scipy", "cython", "numpy", "h5py", "imageio", "argparse"],
+        include_dirs=getInclude(), 
+        packages=find_packages(),        
+        package_data=package_data,
+        ext_modules=exts
     )
 
 
 if __name__ == "__main__":
     # pip install --editable .
-    setup_package()
+    # python setup.py build_ext --inplace
+    no_cython = True
+    if 'build_ext' in sys.argv:
+        no_cython = False
+    setup_package(no_cython)
